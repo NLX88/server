dc644b1d4951c206e075563f724c1f67264ba4fe
Heriberto <heriberto.fortuna461@gmail.com>
2024-12-01 15:51:49 -0600

    Avance en la conexion a la base de datos

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,21 @@
 import  express  from "express";
 import router from "./router";
+import db from "./config/db";
+
+//Conectar a la base de datos
+
+async function ConnectionDB() {
+    try{
+        await db.authenticate()
+        db.sync()
+        console.log("Conexion exitosa a la Base de Datos")
+    }catch(error){
+        console.log(error)
+        console.log("Hubo un error al conectar a la Base de Datos")
+    }
+}
+
+ConnectionDB()
 
 const server = express()
 
