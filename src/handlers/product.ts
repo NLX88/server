a23de6015cef84446446b4607ffd64215b92c057
Heriberto <heriberto.fortuna461@gmail.com>
2024-12-05 11:19:19 -0600

    Errores en el handler

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from "express"
-import {validationResult} from "express-validator"
 import Product from "../models/Product.model"
 
 export const createProduct = async (req: Request, res: Response) => {
 
-    
 
     const product = await Product.create(req.body)
     res.json({data: product})
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { body } from "express-validator"
 import { createProduct } from "./handlers/product"
+import { handleImputErrors } from "./middleware"
 
 const router = Router()
 
@@ -9,7 +10,21 @@ router.get("/",(req,res) => {
     res.json("Desde Get")
 })
 
-router.post("/",createProduct)
+router.post("/",
+   // /*
+    //validacion
+    body("name").notEmpty()
+    .withMessage("EL nombre del producto no puede ser vacio"),
+
+
+    body("price").isNumeric().withMessage("Valor no valido").notEmpty()
+    .withMessage("El precio no puede ir vacio")
+    .custom(value => value > 0).withMessage("El precio no puede ser 0"),
+    //*/
+    handleImputErrors,
+    
+    createProduct
+)
 
 router.put("/",(req,res) => {
     res.json("Desde Put")
