27ddbffd81e3aa26d8d5d20fb5ae3322a76f021f
Heriberto <heriberto.fortuna461@gmail.com>
2024-12-08 22:09:51 -0600

    ok

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("desde index.ts");
+const server_1 = __importDefault(require("./server"));
+const colors_1 = __importDefault(require("colors"));
+server_1.default.listen(4000, () => {
+    console.log(colors_1.default.cyan.bold("REST API en el puerto 4000"));
+});
 //# sourceMappingURL=index.js.map
--- dist/server.d.ts
+++ dist/server.d.ts
@@ -1,1 +1,3 @@
-export declare function sumar(): void;
+export declare function ConnectionDB(): Promise<void>;
+declare const server: import("express-serve-static-core").Express;
+export default server;
