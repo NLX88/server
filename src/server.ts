54af32a8ae8e8b8c4f951fae238c5c10540df850
Heriberto <heriberto.fortuna461@gmail.com>
2024-12-08 21:07:32 -0600

    ok

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import  express  from "express";
 import colors from "colors";
+import cors, {CorsOptions} from "cors"
+import morgan from "morgan"
 import swaggerUi from "swagger-ui-express"
 import swaggerSpect from "./config/swagger";
 import router from "./router";
@@ -25,9 +27,23 @@ ConnectionDB()
 //instancia de express
 const server = express()
 
+//permitir conexiones 
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback){
+        if(origin === process.env.FRONTEND_URL){
+            callback(null,true)
+        }else{
+            callback(new Error("Error de CORS"))
+        }
+    }
+}
+
+server.use(cors(corsOptions))
+
 //leer datos de formularios 
 server.use(express.json())
 
+server.use(morgan("dev"))
 
 server.use("/api/products", router)
 
